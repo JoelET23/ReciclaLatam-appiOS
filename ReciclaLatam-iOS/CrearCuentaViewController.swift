e8d018a84938c7624bf99c1cba9b4f82eed6619c
JoelET23 <joelfabianelmertello@gmail.com>
2024-11-14 13:07:56 -0800

    Initial Commit

---
+++ ReciclaLatam-iOS/CrearCuentaViewController.swift
@@ -0,0 +1,21 @@
+//
+//  CrearCuentaViewController.swift
+//  ReciclaLatam-iOS
+//
+//  Created by Joel Elmer Tello on 26/11/24.
+//
+
+import UIKit
+
+class CrearCuentaViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Uncomment the following line to preserve selection between presentations
+        // self.clearsSelectionOnViewWillAppear = false
+
+        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
+        // self.navigationItem.rightBarButtonItem = self.editButtonItem
+    }
+}
